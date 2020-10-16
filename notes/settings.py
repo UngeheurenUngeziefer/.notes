88739897255640bfe1c24dce8c7587bfc3e640d0
UngeheurenUngeziefer <seweruskein2@gmail.com>
2020-10-16 23:51:24 +0300

    deplooy

--- notes/settings.py
+++ notes/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': str(BASE_DIR/'db.sqlite3'),
     }
 }
 
