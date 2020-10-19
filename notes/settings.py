72ea93b7f485295789ba22449a080a1151863503
jleaniuk <jauhien_leaniuk@gmail.com>
2020-10-19 15:12:08 +0300

    asd

--- notes/settings.py
+++ notes/settings.py
@@ -129,18 +129,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    normpath(join(BASE_DIR, 'static')),
-    normpath(join(BASE_DIR, 'upload')),
-)
-
-
 
 
 # Email settings
