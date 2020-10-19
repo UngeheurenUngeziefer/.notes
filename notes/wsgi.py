7a6b90adb32d1a99e1238bc2a527da3b86c0a8d0
jleaniuk <jauhien_leaniuk@gmail.com>
2020-10-19 14:37:57 +0300

    asd

--- notes/wsgi.py
+++ notes/wsgi.py
@@ -8,11 +8,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notes.settings')
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notes.settings")
+application = Cling(get_wsgi_application())
