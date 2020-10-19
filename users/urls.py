006a131653cbb9a5448ab916c5dc62db111cdc85
jleaniuk <jauhien_leaniuk@gmail.com>
2020-10-19 19:58:06 +0300

    activation

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,10 @@ from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
+	url(r'^', include('django.contrib.auth.urls')),
 	url(r'^login', LoginView.as_view(template_name='users/login.html'), name='login'),
 	url(r'^logout', views.logout_view, name='logout'),
 	url(r'^register/', views.register, name='register'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,50})/$',
-     views.activate, name='activate')
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$',
+        views.activate, name='activate'),
 ]
