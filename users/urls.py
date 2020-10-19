716b0e0f2e3f5eb85da88b8bd602da3f1c3ba621
jleaniuk <jauhien_leaniuk@gmail.com>
2020-10-19 16:14:29 +0300

    $ dropped

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,9 @@ from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
-	url(r'^login$', LoginView.as_view(template_name='users/login.html'), name='login'),
-	url(r'^logout$', views.logout_view, name='logout'),
-	url(r'^register/$', views.register, name='register'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 
+	url(r'^login', LoginView.as_view(template_name='users/login.html'), name='login'),
+	url(r'^logout', views.logout_view, name='logout'),
+	url(r'^register/', views.register, name='register'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', 
     	views.activate, name='activate'),
 ]
