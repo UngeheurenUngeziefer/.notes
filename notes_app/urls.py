ee894b4f3eda7f44f43f6672c16709cf10a7bc25
jleaniuk <jauhien_leaniuk@gmail.com>
2020-10-20 14:11:43 +0300

    path added

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
 	url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 	url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 	url(r'^delete_entry/(?P<entry_id>\d+)/$', views.delete_entry, name='delete_entry'),
+	path('users/', include('users.urls', namespace="users")),
 ]
