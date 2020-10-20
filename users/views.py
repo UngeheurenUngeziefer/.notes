3eb6c8dca0467dcc682a317a04943a761ce2b60f
jleaniuk <jauhien_leaniuk@gmail.com>
2020-10-20 14:07:25 +0300

    decode removed

--- users/views.py
+++ users/views.py
@@ -36,11 +36,11 @@ def register(request):
             #     'token': account_activation_token.make_token(user),
             # })
 
-
+            
             message = render_to_string('users/acc_active_email.html', {
                 'user': user,
                 'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': account_activation_token.make_token(user),
             })
 
